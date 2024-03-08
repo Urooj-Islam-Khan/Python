1b61e406a389f66af22a1220392b3f5b58eefd85
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-03-08 14:29:36 +0500

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,10 @@
+num = int(input("Enter value of number :"))
+
+if num < 0:
+    print("It is negative number")
+
+elif num == 0:
+    print("It is zero")
+
+else:
+    print("It is positive number")
