665984d67d7a88d32c7d5373e3532acb4f09f869
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-03-08 14:29:16 +0500

    Add files via upload

---
+++ 1stProg.py
@@ -0,0 +1,39 @@
+# Syntax to print string
+print("Hello World")
+
+# Syntax to print number
+print(4)
+
+# Syntax to print character
+print('A')
+
+# Syntax to print string and number together
+print ("My age is " , 19)
+
+# Arithmetic
+# Syntax to add two numbers
+print(5 + 4)
+
+# Syntax to sub two numbers
+print(7 - 4)
+
+# Syntax to multiply two numbers
+print(10 * 3)
+
+# Syntax to divide two numbers
+print(20 / 4)
+
+# Syntax to initialize variable
+age = 19
+
+# Syntax to print statement with variable
+print("I am ", age, "years old")
+
+# Syntax to take input
+name = input("Enter your name ")
+
+# Syntax to call input
+print("Your name is", name)
+
+# Syntax to print escape sequence {\n,  \t,  \b,   \"}
+print("I am Urooj \n Student of Software Engineering ")
